4a8649ac82e7321e46728c4d8e832c444f2e3b65
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 14:51:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-// global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let meanId = 0;
+class Meal {
+  constructor(title, price){
+    this.id = ++meanId;
+    this.title = title;
+    this.price = price;
+  }
+}
