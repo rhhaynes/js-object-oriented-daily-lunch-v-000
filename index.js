7b18d31813d6268f0a54bfa5548ab49bec866016
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 14:53:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
+let neighborhoodId = 0;
+class Neighborhood {
+  constructor(name){
+    this.id = ++neighborhoodId;
+    this.name = name;
+    store.neighborhoods.push(this);
+  }
+}
+
 let mealId = 0;
 class Meal {
   constructor(title, price){
     this.id = ++mealId;
     this.title = title;
     this.price = price;
+    store.meals.push(this);
   }
 }
