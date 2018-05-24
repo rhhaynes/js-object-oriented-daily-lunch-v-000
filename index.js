cc7a2835471bdc19a89572cf8388c43b5da0fe2c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 15:40:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ class Meal {
   }
   deliveries(){ return store.deliveries.filter( d => d.mealId === this.id ) }
   customers(){ return this.deliveries().map( d => d.customer() ) }
-  static byPrice(){ return [...new Set()].sort( (a, b) => a ) }
+  static byPrice(){ return [...new Set(store.meals())].sort( (a, b) => a - b ) }
 }
 
 let deliveryId = 0;
